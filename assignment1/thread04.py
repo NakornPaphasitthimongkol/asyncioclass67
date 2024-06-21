2e84011dc0514b1eb5185d906b08e7604b24dc23
NakornPaphasitthimongkol <108925445+NakornPaphasitthimongkol@users.noreply.github.com>
2024-06-21 12:03:35 +0700

    Update thread04.py

--- assignment1/thread04.py
+++ assignment1/thread04.py
@@ -1,1 +1,19 @@
 # extending the Thread class and return values
+from time import sleep, ctime
+from threading import Thread
+
+class CustomThread(Thread):
+    def run(self):
+        sleep(1)
+        print(f'{ctime()} This is from another thread')
+        self.value = 99
+
+thread = CustomThread()
+
+thread.start()
+
+print(f'{ctime()} Waiting for the thread...')
+thread.join()
+
+value = thread.value
+print(f'{ctime()} Got: {value}')
