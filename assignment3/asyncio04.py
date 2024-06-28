c13236ef986b76543923115001e0dc2d1f328ef3
Nakorn-P <6410301027@cdti.ac.th>
2024-06-28 11:51:43 +0700

    add

--- assignment3/asyncio04.py
+++ assignment3/asyncio04.py
@@ -1,1 +1,9 @@
 # Starting task
+from time import ctime
+import asyncio
+
+async def wash(basket):
+    print(f'{ctime()}   : Washing Machine ({basket}): Put the coin')
+    print(f'{ctime()}   : Washing Machine ({basket}): Start washing...')
+    await asyncio.sleep(5)
+    
